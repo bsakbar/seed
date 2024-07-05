d51a29d0055be03462044567a4dd0d649687b2c1
bsakbar <batool.s.ak@gmail.com>
2024-07-05 14:39:40 -0400

    alts

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="logo">
-      <img src='/logo4.svg' height='131'/>
-      <img src='/title.svg' height='56'/>
+      <img src='/logo4.svg' height='131' alt='think_coffee_logo'/>
+      <img src='/title.svg' height='56' alt='title_text'/>
       <span className='instructions'>Click on elements to turn them on and off</span>
     </div>
   );
